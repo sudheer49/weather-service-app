39d04c093075b3a8bf76b26ed809fcfbe592b2ea
Satya Sudheer Kolipaka <satya_kolipaka@infosys.com>
2021-02-07 11:55:56 +0100

    Basic project code

---
+++ src/main/java/com/tenera/weatherService/WeatherServiceAppApplication.java
@@ -0,0 +1,13 @@
+package com.tenera.weatherService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherServiceAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherServiceAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tenera/weatherService/WeatherServiceAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tenera.weatherService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherServiceAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
