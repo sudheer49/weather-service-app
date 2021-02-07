ea745114132e059e772885f9350b82d669e1bd7a
Satya Sudheer Kolipaka <satya_kolipaka@infosys.com>
2021-02-07 12:02:22 +0100

    Rename main class

--- src/main/java/com/tenera/weatherService/WeatherServiceApplication.java
+++ src/main/java/com/tenera/weatherService/WeatherServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WeatherServiceAppApplication {
+public class WeatherServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(WeatherServiceAppApplication.class, args);
+		SpringApplication.run(WeatherServiceApplication.class, args);
 	}
 
 }
